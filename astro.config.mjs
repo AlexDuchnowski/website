711c6aeab7626efc436e5a9775412d73ee28620a
AlexDuchnowski <alex_duchnowski@brown.edu>
2025-07-15 10:51:52 -0400

    make image components more Astro-friendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,27 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-    site: "https://AlexDuchnowski.github.io",
-    base: "/website",
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `@use "/src/styles" as *;`,
-                },
-            },
-        },
-    },
-    markdown: {
-        shikiConfig: {
-            // theme: "monokai",
-            theme: "material-theme-darker",
-        },
-    },
+  site: "https://AlexDuchnowski.github.io",
+  base: "/website",
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: `@use "/src/styles" as *;`,
+              },
+          },
+      },
+  },
+
+  markdown: {
+      shikiConfig: {
+          // theme: "monokai",
+          theme: "material-theme-darker",
+      },
+  },
+
+  integrations: [mdx()],
 });
