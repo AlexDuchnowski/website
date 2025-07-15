0b9d1d6dfd1800ac0253dbe758fe8dcb8d4c374d
AlexDuchnowski <alex_duchnowski@brown.edu>
2025-07-14 20:01:26 -0400

    prepping for github hosting + deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
+    site: "https://AlexDuchnowski.github.io",
+    base: "/website",
     vite: {
         css: {
             preprocessorOptions: {
